31d65acd5f11c4053f0d0d688450152a0a09ca83
tdaygreene <tdaygreene@gmail.com>
2025-01-20 18:11:06 -0600

    setup FPS and draw player functional

--- main.py
+++ main.py
@@ -3,9 +3,13 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     print ("Starting asteroids!")
     print ("Screen width: 1280") 
     print ("Screen height: 720")
@@ -15,8 +19,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
-    
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
