77dad490867feed5c49793b26cdfee1bbfd526ff
tdaygreene <tdaygreene@gmail.com>
2025-01-21 20:33:57 -0600

    shoot method implemented, bullets spawn, no collision or rotation

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
+import sys
 
 def main():
     pygame.init()
@@ -13,10 +15,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable,)
+    Shot.containers = (shots, updatable, drawable)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
@@ -34,11 +38,17 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill((0,0,0))
+
         for thing in updatable:
             thing.update(dt)
+        for thing in asteroids:
+            if thing.collision_check(player) == True:
+                sys.exit("Game Over!")
         for thing in drawable:
             thing.draw(screen)
+            
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
