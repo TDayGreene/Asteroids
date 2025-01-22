32d6122d46cded0542d7250e3602b874699a1390
tdaygreene <tdaygreene@gmail.com>
2025-01-21 18:55:16 -0600

    asteroids render and move, no collision

--- main.py
+++ main.py
@@ -4,15 +4,26 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 def main():
     pygame.init()
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    ast_field = AsteroidField()
+
     print ("Starting asteroids!")
     print ("Screen width: 1280") 
     print ("Screen height: 720")
